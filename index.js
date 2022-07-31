251e85040e772cf7c8c9cc4a107b64f4ab9a3035
Bouncyyahomie <thornny.s@gmail.com>
2022-07-31 18:33:56 +0700

    config payload

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ const client = new line.Client(lineConfig)
 const app = express()
 const PORT = process.env.PORT || 3000 //
 
+lineID_sent_to = ["111", "222"]
+
 app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
+    try{
+      const events = req.body.events
+      console.log('event=>>>>', events)
+      return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK")
+    }catch(error){
+      res.status(500).end()
+    }
+})
+
+app.post("/alert", async(req, res) => {
     // content = request.get_json()
     // alert_title = content['alert_title']
     // alert_query = content['alert_query']
@@ -24,14 +36,216 @@ app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
     // link = content['link']
     // image = content['snapshot']
     // tags = content['tags']
-    try{
-      const events = req.body.events
-      console.log('event=>>>>', events)
-      return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK")
-    }catch(error){
-      res.status(500).end()
+    
+    const { alert_title, alert_query, event_title, alert_type, priority, link, snapshot, tags } = req.body
+
+    strUrl = "https://api.line.me/v2/bot/message/push"
+    // curl -v -X POST https://api.line.me/v2/bot/message/multicast \
+    // -H 'Content-Type: application/json' \
+    // -H 'Authorization: Bearer {channel access token}' \
+    // -d '{
+    //     "to": ["U4af4980629...","U0c229f96c4..."],
+    //     "messages":[
+    //         {
+    //             "type":"text",
+    //             "text":"Hello, world1"
+    //         },
+    //         {
+    //             "type":"text",
+    //             "text":"Hello, world2"
+    //         }
+    //     ]
+    // }'
+
+    message = mm_set(event_title, alert_tile, alert_type, tags, priority, link, snapshot)
+
+    const res = await axios.post('"https://api.line.me/v2/bot/message/push', 
+      { 
+        "to": ["Ubedd0f50b99217db43961d4fded59241"],
+        "messages": message
+      }, 
+      {
+        headers: {
+          // 'application/json' is the modern content-type for JSON, but some
+          // older servers may use 'text/json'.
+          // See: http://bit.ly/text-json
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${channelAccessToken}`
+        }
     }
+    );
 })
+
+function mm_set(event_title, alert_tile, alert_type, tags, priority, link, snapshot) {
+  return message = {
+    "type": "bubble",
+    "hero": {
+      "type": "image",
+      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+      "size": "full",
+      "aspectRatio": "20:13",
+      "aspectMode": "cover",
+      "action": {
+        "type": "uri",
+        "uri": snapshot
+      }
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "Datadog Monitor",
+          "weight": "bold",
+          "size": "xl"
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "lg",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Event",
+                  "color": "#aaaaaa",
+                  "size": "sm",
+                  "flex": 1
+                },
+                {
+                  "type": "text",
+                  "text": event_title,
+                  "wrap": true,
+                  "color": "#666666",
+                  "size": "sm",
+                  "flex": 5
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Alert title",
+                  "color": "#aaaaaa",
+                  "size": "sm",
+                  "flex": 1
+                },
+                {
+                  "type": "text",
+                  "text": alert_title,
+                  "wrap": true,
+                  "color": "#666666",
+                  "size": "sm",
+                  "flex": 5
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Alert type",
+                  "color": "#aaaaaa",
+                  "size": "sm",
+                  "flex": 1
+                },
+                {
+                  "type": "text",
+                  "text": alert_type,
+                  "wrap": true,
+                  "color": "#666666",
+                  "size": "sm",
+                  "flex": 5
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Tags",
+                  "color": "#aaaaaa",
+                  "size": "sm",
+                  "flex": 1
+                },
+                {
+                  "type": "text",
+                  "text": tags,
+                  "wrap": true,
+                  "color": "#666666",
+                  "size": "sm",
+                  "flex": 5
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Priority",
+                  "color": "#aaaaaa",
+                  "size": "sm",
+                  "flex": 1
+                },
+                {
+                  "type": "text",
+                  "text": priority,
+                  "wrap": true,
+                  "color": "#666666",
+                  "size": "sm",
+                  "flex": 5
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "sm",
+      "contents": [
+        {
+          "type": "button",
+          "style": "link",
+          "height": "sm",
+          "action": {
+            "type": "uri",
+            "label": "WEBSITE",
+            "uri": link
+          }
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [],
+          "margin": "sm"
+        }
+      ],
+      "flex": 0
+    }
+  }
+}
 
 const handleEvent = async (event) =>{
   console.log(event)
